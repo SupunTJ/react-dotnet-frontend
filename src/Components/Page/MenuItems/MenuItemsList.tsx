aee8cc0af1f35ffb05fc25cfabc09f0cef7fb681
SupunTJ <jayaweeraspn@gmail.com>
2024-05-29 23:42:40 +0530

    displaying menu item details

--- src/Components/Page/MenuItems/MenuItemsList.tsx
+++ src/Components/Page/MenuItems/MenuItemsList.tsx
@@ -2,23 +2,37 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { menuItemModel } from "../../../Interfaces";
 import MenuItemCard from "./MenuItemCard";
+import { useGetMenuItemsQuery } from "../../../Apis/menuItemApi";
+import { useDispatch } from "react-redux";
+import { setMenuItem } from "../../../Storage/Redux/menuItemSclice";
 
 function MenuItemsList() {
-  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+  // const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+
+  const dispatch = useDispatch();
+  const { data, isLoading } = useGetMenuItemsQuery(null);
 
   useEffect(() => {
-    fetch("https://redmangoapi.azurewebsites.net/api/MenuItem")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setMenuItems(data.result); // result is the array of data
-      });
-  }, []);
+    // fetch("")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     setMenuItems(data.result); // result is the array of data
+    //   });
+    if (!isLoading) {
+      dispatch(setMenuItem(data.result));
+    }
+  }, [isLoading]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="container row">
       {/* conditional rendering */}
-      {menuItems.length > 0 &&
-        menuItems.map((menuItem, index) => (
+      {data.result.length > 0 &&
+        data.result.map((menuItem: menuItemModel, index: number) => (
           <MenuItemCard menuItem={menuItem} key={index} />
         ))}
     </div>
