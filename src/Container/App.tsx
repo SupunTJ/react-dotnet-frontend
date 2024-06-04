1fb7320d5631d77c7b1d34877e9ba09a3e248d8e
SupunTJ <jayaweeraspn@gmail.com>
2024-06-04 13:23:19 +0530

    solving cart loading issue

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Footer, Header } from "../Components/Layout";
 import {
   AccessDenied,
@@ -18,30 +18,24 @@ import {
   Register,
   ShoppingCart,
 } from "../Pages";
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useGetShoppingCartQuery } from "../Apis/shoppingCartApi";
 import { setShoppingCart } from "../Storage/Redux/shoppingCartSlice";
-import { userModel } from "../Interfaces";
 import jwt_decode from "jwt-decode";
-import { setLoggedInUser } from "../Storage/Redux/userAuthSclice";
+import { userModel } from "../Interfaces";
+
 import { RootState } from "../Storage/Redux/store";
+import { setLoggedInUser } from "../Storage/Redux/userAuthSclice";
 
 function App() {
   const dispatch = useDispatch();
-  const userData: userModel = useSelector(
-    (state: RootState) => state.userAuthStore
-  );
-
-  const { data, isLoading } = useGetShoppingCartQuery(userData.id);
-
-  useEffect(() => {
-    if (!isLoading) {
-      // console.log(data.result);
-      dispatch(setShoppingCart(data.result?.cartItems));
-    }
-  }, [data]);
+  const [skip, setSkip] = useState(true);
+  const userData = useSelector((state: RootState) => state.userAuthStore);
+  const { data, isLoading } = useGetShoppingCartQuery(userData.id, {
+    skip: skip,
+  });
 
   useEffect(() => {
     const localToken = localStorage.getItem("token");
@@ -49,7 +43,18 @@ function App() {
       const { fullName, id, email, role }: userModel = jwt_decode(localToken);
       dispatch(setLoggedInUser({ fullName, id, email, role }));
     }
-  });
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading && data) {
+      console.log(data);
+      dispatch(setShoppingCart(data.result?.cartItems));
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (userData.id) setSkip(false);
+  }, [userData]);
 
   return (
     <div>
@@ -62,8 +67,8 @@ function App() {
             element={<MenuItemDetails />}
           ></Route>
           <Route path="/shoppingCart" element={<ShoppingCart />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
           <Route
             path="/authentication"
@@ -73,34 +78,24 @@ function App() {
             path="/authorization"
             element={<AuthorizationTestAdmin />}
           ></Route>
-          <Route path="/accessDenied" element={<AccessDenied />}></Route>
-          <Route path="/payment" element={<Payment />}></Route>
+          <Route path="/accessDenied" element={<AccessDenied />} />
+          <Route path="/payment" element={<Payment />} />
           <Route
-            path="/order/orderConfirmed/:id"
+            path="order/orderconfirmed/:id"
             element={<OrderConfirmed />}
           ></Route>
-          <Route path="/order/myOrders" element={<MyOrders />}></Route>
-          <Route
-            path="/order/orderDetails/:id"
-            element={<OrderDetails />}
-          ></Route>
-          <Route path="/order/allOrders" element={<AllOrders />}></Route>
-          <Route
-            path="/menuItem/menuItemList"
-            element={<MenuItemList />}
-          ></Route>
+          <Route path="/order/myOrders" element={<MyOrders />} />
+          <Route path="/order/orderDetails/:id" element={<OrderDetails />} />
+          <Route path="/order/allOrders" element={<AllOrders />} />
+          <Route path="/menuItem/menuitemlist" element={<MenuItemList />} />
           <Route
             path="/menuItem/menuItemUpsert/:id"
             element={<MenuItemUpsert />}
-          ></Route>
-          <Route
-            path="/menuItem/menuItemUpsert"
-            element={<MenuItemUpsert />}
-          ></Route>
+          />
+          <Route path="/menuItem/menuItemUpsert" element={<MenuItemUpsert />} />
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </div>
-
       <Footer />
     </div>
   );
